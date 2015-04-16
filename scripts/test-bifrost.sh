7615939bf16c759725efc27cfaa9f91abfc9e5c4
Julia Kreger <juliaashleykreger@gmail.com>
2015-04-16 15:55:57 -0400

    Move env-setup to /scripts and add test script
    Created and moved scripts into a scripts folder to keep things tidy.
    
    Added a basic test script which invokes env-setup.sh, loads the
    Ansible environment, and then initiates the test playbook.

---
+++ scripts/test-bifrost.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+SCRIPT_HOME=$(dirname $0)
+BIFROST_HOME=$SCRIPT_HOME/..
+# Install Ansible
+$SCRIPT_HOME/env-setup.sh
+
+# Source Ansible
+source /opt/stack/ansible/hacking/env-setup
+
+# Change working directory
+cd $BIFROST_HOME/playbooks
+
+# Execute test playbook
+ansible-playbook -vvvv -i inventory/localhost test-bifrost.yaml
