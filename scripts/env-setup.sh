33cdd0fcc629801e4027e1a03a092d363b232e9f
Julia Kreger <juliaashleykreger@gmail.com>
2016-01-15 10:52:32 -0500

    Ansible 2.0 change and module cleanup
    Added a new release note highlighting the change to Ansible 2.0,
    as well as removal of modueles as they were included with the
    release of Ansible 2.0.
    
    Additionally added a mechanism to pull the modules from the Ansible
    stable-2.0 branch should they be missing, largely in event of an
    ansible 1.9.x user.
    
    Change-Id: I0abd9bacfab020fdc7b94bb086375c27e4987693

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-set -e
+set -eu
 
 ANSIBLE_GIT_URL=${ANSIBLE_GIT_URL:-https://github.com/ansible/ansible.git}
 # Note(TheJulia): Normally this should be stable-2.0, pinning due to
 # issues with the stable branch.
 # https://github.com/ansible/ansible-modules-core/issues/2804
 ANSIBLE_GIT_BRANCH=${ANSIBLE_GIT_BRANCH:-v2.0.0.0-1}
+
+function check_get_module () {
+    local file=${1}
+    local url=${2}
+    if [ ! -e ${file} ]; then
+        wget -O ${file} ${url}
+    fi
+}
 
 if [ -x '/usr/bin/apt-get' ]; then
     if ! $(gcc -v &>/dev/null); then
@@ -58,6 +66,7 @@ cd /opt/stack
 
 if [ ! -d ansible ]; then
     git clone $ANSIBLE_GIT_URL --recursive -b $ANSIBLE_GIT_BRANCH
+    cd ansible
 else
     cd ansible
     git checkout $ANSIBLE_GIT_BRANCH
@@ -65,6 +74,15 @@ else
     git submodule update --init --recursive
     git fetch
 fi
+# Note(TheJulia): These files should be in the ansible folder
+# and this functionality exists for a level of ansible 1.9.x
+# backwards compatability although the modules were developed
+# for Ansible 2.0.
+
+check_get_module `pwd`/lib/ansible/modules/core/cloud/openstack/os_ironic.py \
+    https://raw.githubusercontent.com/ansible/ansible-modules-core/stable-2.0/cloud/openstack/os_ironic.py
+check_get_module `pwd`/lib/ansible/modules/core/cloud/openstack/os_ironic_node.py \
+    https://raw.githubusercontent.com/ansible/ansible-modules-core/stable-2.0/cloud/openstack/os_ironic_node.py
 
 echo
 echo "If you're using this script directly, execute the"
