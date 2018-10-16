openstack/bifrost

commit 431b76892936c4e228b42e51916000b6ffbb60ac

scripts: Fix shell expansion when collecting VM logs
The [[:digit:]] does not work correctly when using sudo

 2018-10-04 14:31:33.319558 | opensuse-423 | + sudo cp '/var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log' /home/zuul/workspace/logs
 2018-10-04 14:31:33.328756 | opensuse-423 | cp: cannot stat '/var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log': No such file or directory

As we can see [[:digit:]] is not expanded so no logs are collected. This
is because the expansion happens by the user but since he does not have
access to the directory the command fails. We can fix this by opening a
subshell so it gets run with sudo privileges.

Change-Id: Ibdf8606a3b72c48faffdf2b58b122b690008ba98

scripts/collect-test-info.sh
@@ -14,7 +14,7 @@ echo "Making logs directory and collecting logs."
 [ -d ${LOG_LOCATION} ] || mkdir -p ${LOG_LOCATION}
 
 if [ -z "${TEST_VM_NODE_NAMES+x}" ]; then
-    sudo cp /var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log ${LOG_LOCATION}
+    sudo sh -c "cp /var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log" ${LOG_LOCATION}
     sudo chown $USER ${LOG_LOCATION}/testvm[[:digit:]]_console.log
     sudo chmod o+r ${LOG_LOCATION}/testvm[[:digit:]]_console.log
 else

commit 6db1bee4c337b209f6c580428653fd49e5621fa1

scripts: collect-test-info: Fix cp command when collecting VM logs
Fixes: 431b76892936 ("scripts: Fix shell expansion when collecting VM logs")
Change-Id: I4edf4eeb9118df8c2bd219d23bde85a74e1375c5

scripts/collect-test-info.sh
@@ -14,7 +14,7 @@ echo "Making logs directory and collecting logs."
 [ -d ${LOG_LOCATION} ] || mkdir -p ${LOG_LOCATION}
 
 if [ -z "${TEST_VM_NODE_NAMES+x}" ]; then
-    sudo sh -c "cp /var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log" ${LOG_LOCATION}
+    sudo sh -c "cp /var/log/libvirt/baremetal_logs/testvm[[:digit:]]_console.log ${LOG_LOCATION}"
     sudo chown $USER ${LOG_LOCATION}/testvm[[:digit:]]_console.log
     sudo chmod o+r ${LOG_LOCATION}/testvm[[:digit:]]_console.log
 else